8f75e67c0f2cc41c04103eb6c1b7a0e99214c83c
KozubIgn <ignacykozub@gmail.com>
2021-02-23 15:45:28 +0100

    Add get methods for user

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const pool = require("./db");
+const user = require("./routes/user");
 
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
-
+app.use('/', user);
 
 // get announcements test
 app.get("/announcements", async (req, res) => {
@@ -15,6 +16,7 @@ app.get("/announcements", async (req, res) => {
         console.log(err.message);
     }
 })
+
 
 
 
---
+++ routes/user.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const pool = require("../db");
+
+router.get("/users", async (req, res) => {
+    try {
+        const users = await pool.query("SELECT * FROM users");
+        res.json(users.rows);
+    } catch (err) {
+        console.log(err.message);
+    }
+})
+router.get("/users/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await pool.query("SELECT * FROM users WHERE user_id = $1", [id]);
+        res.json(user.rows[0]);
+    } catch (err) {
+        console.log(err.message);
+    }
+})
+
+module.exports = router ;
