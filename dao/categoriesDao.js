e999f08882c57196c7d8161d0b2d9a240fd59234
KozubIgn <ignacykozub@gmail.com>
2021-02-25 12:34:56 +0100

    get allcategories method

--- dao/categoriesDao.js
+++ dao/categoriesDao.js
@@ -2,7 +2,14 @@ const pool = require("./poolDb");
 
 const categoriesDao = {
     getAllCategories: async () => {
-        const queryResult = await pool.query("WITH RECURSIVE (id, category_name, parent_id) AS (SELECT id, category_name, ARRAY[parent_id] FROM categories WHERE parent_id IS NULL UNION ALL SELECT categories.id, categories.category_name, parent_id || categories.id FROM category_tree JOIN categories ON categories.parent_id=category_tree.id WHERE NOT categories.id = ANY(parent_id)) SELECT * FROM category_tree ORDER BY id;)");
+        const queryResult = await pool.query("SELECT * from categories");
+        // const queryResult = await pool.query("WITH RECURSIVE category_tree (id, category_name, parent) " +
+        //     "AS (SELECT id, category_name, ARRAY[id] FROM categories WHERE parent_id IS NULL " +
+        //     "UNION ALL SELECT categories.id, categories.category_name, parent || categories.id FROM category_tree " +
+        //     "JOIN categories " +
+        //     "ON categories.parent_id=category_tree.id " +
+        //     "WHERE NOT categories.id = ANY(parent)) " +
+        //     "SELECT * FROM category_tree ORDER BY id");
         return queryResult.rows;
     },
 }
