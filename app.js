4e0ce4d86b3586dca82d95a53240dbdc362dbbbe
KozubIgn <ignacykozub@gmail.com>
2021-03-02 14:25:28 +0100

    move routes into the user.js

--- app.js
+++ app.js
@@ -9,74 +9,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/', user);
 
-
-app.post("/users", async (req, res) => {
-    console.log(req.body.email);
-    try {
-        let usertoAdd = {
-            user_email: req.body.email,
-            user_password: req.body.password,
-            pi_firstname: req.body.firstname,
-            pi_lastname: req.body.lastname
-        };
-        const user = await userDao.addUser(usertoAdd);
-        res.json(user);
-    } catch (err) {
-        console.log(err.message);
-    }
-})
-
-
-app.get("/id",async (req, res) =>{
-    try{
-       const id = await userDao.getLastUserId();
-       res.json(id)
-    }catch(err){
-        console.log(err.message);
-    }
-})
-app.post("/users/:id/address", async (req, res) => {
-    try {
-        let address = {
-            street: req.body.street,
-            buildingNumber: req.body.buildingNumber,
-            flatNumber: req.body.flatNumber,
-            postalCode: req.body.postalCode,
-            city: req.body.city,
-            country: req.body.country
-        };
-        const id = req.params.id;
-        const userAddress = await userDao.addUserAddressById(id, address);
-        res.json(userAddress);
-    } catch (err) {
-        console.log(err.message);
-    }
-});
-
-app.get("/id", async (req, res) => {
-    try {
-        const id = await userDao.getLastUserId();
-        res.json(id)
-    } catch (err) {
-        console.log(err.message);
-    }
-});
-
 // get announcements test
 app.get("/announcements", async (req, res) => {
     try {
         const announcements = await pool.query("SELECT * FROM announcements");
         res.json(announcements.rows);
-    } catch (err) {
-        console.log(err.message);
-    }
-});
-
-app.delete("/users/:id", async (req, res) => {
-    try {
-        const {id} = req.params
-        await userDao.deleteUserById(id);
-        res.send("User Deleted !")
     } catch (err) {
         console.log(err.message);
     }
--- routes/user.js
+++ routes/user.js
@@ -9,15 +9,63 @@ router.get("/users", async (req, res) => {
     } catch (err) {
         console.log(err.message);
     }
-})
+});
+
+router.post("/users", async (req, res) => {
+    console.log(req.body.email);
+    try {
+        let usertoAdd = {
+            user_email: req.body.email,
+            user_password: req.body.password,
+            pi_firstname: req.body.firstname,
+            pi_lastname: req.body.lastname
+        };
+        const user = await userDao.addUser(usertoAdd);
+        res.json(user);
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
 router.get("/users/:id", async (req, res) => {
     try {
-        const { id } = req.params;
+        const {id} = req.params;
         const user = await pool.query("SELECT * FROM users WHERE user_id = $1", [id]);
         res.json(user.rows[0]);
     } catch (err) {
         console.log(err.message);
     }
-})
+});
 
-module.exports = router ;
+// post user address
+router.post("/users/:id/address", async (req, res) => {
+    try {
+        let address = {
+            street: req.body.street,
+            buildingNumber: req.body.buildingNumber,
+            flatNumber: req.body.flatNumber,
+            postalCode: req.body.postalCode,
+            city: req.body.city,
+            country: req.body.country
+        };
+        const id = req.params.id;
+        const userAddress = await userDao.addUserAddressById(id, address);
+        res.json(userAddress);
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
+router.delete("/users/:id", async (req, res) => {
+    try {
+        const {id} = req.params
+        await userDao.deleteUserById(id);
+        res.send("User Deleted !")
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
+
+//TODO: update user details(pass, email. name, address, phone etc.)
+module.exports = router;
